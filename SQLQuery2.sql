7ef9c6bd6e1e21bc42f023003f9470a3e5aa2ee4
Manu Shrivastava <manu.shrivastava@dcmail.ca>
2024-07-05 23:54:32 -0400

    Add files via upload
    SQL file

---
+++ SQLQuery2.sql
@@ -0,0 +1,10 @@
+--USE Student_info;
+--CREATE TABLE students (
+--    student_id INT PRIMARY KEY IDENTITY(1,1),
+--    first_name VARCHAR(50),
+--    last_name VARCHAR(50),
+--    dob DATE,
+--    amount_due DECIMAL(10, 2)
+--);
+
+select * from dbo.students;
